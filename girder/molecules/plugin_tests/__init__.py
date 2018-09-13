44cd4c9cf9a0f5f428d0ac485d6f5a7a8f8133da
Chris Harris <chris.harris@kitware.com>
2018-09-13 10:23:45 -0400

    Revert "Molecule: added test to create molecule via fileId"
    This reverts commit 1021ead853d659572273206a6b307372ac9f4e17.

--- girder/molecules/plugin_tests/__init__.py
+++ girder/molecules/plugin_tests/__init__.py
@@ -19,12 +19,6 @@
 
 import pytest
 import os
-import six
-
-from girder.models.file import File
-from girder.models.folder import Folder
-from girder.models.upload import Upload
-
 
 @pytest.fixture
 def molecule(user):
@@ -64,7 +58,6 @@ def molecule(user):
     # Delete mol
     Molecule().remove(mol)
 
-
 @pytest.fixture
 def calculation(user, molecule):
     """Our method for creating a calculation within girder."""
@@ -95,33 +88,3 @@ def calculation(user, molecule):
 
     # Delete calc
     Calculation().remove(calc)
-
-
-@pytest.fixture
-def make_girder_file():
-    files = []
-
-    def _make_girder_file(assetstore, user, name, contents=b''):
-        folder = Folder().find({
-            'parentId': user['_id'],
-            'name': 'Public'
-        })[0]
-        upload = Upload().uploadFromFile(
-            six.BytesIO(contents), size=len(contents),
-            name=name, parentType='folder', parent=folder,
-            user=user, assetstore=assetstore)
-        # finalizeUpload() does not get called automatically if the contents
-        # are empty
-        if not contents:
-            file = Upload().finalizeUpload(upload, assetstore)
-        else:
-            file = upload
-
-        files.append(file)
-
-        return file
-
-    yield _make_girder_file
-
-    for file in files:
-        File().remove(file)
