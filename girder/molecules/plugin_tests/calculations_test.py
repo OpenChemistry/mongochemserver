e26b7f05a3a3ccd6aba9ef53e8b11b62e5ab82a2
Alessandro Genova <alessandro.genova@kitware.com>
2018-11-01 16:11:26 -0400

    Fix failing tests

--- girder/molecules/plugin_tests/calculations_test.py
+++ girder/molecules/plugin_tests/calculations_test.py
@@ -41,7 +41,8 @@ def test_create_calc(server, molecule, user):
     properties = {
         'molecular mass': 30.0690,
         'melting point': -172,
-        'boiling point': -88
+        'boiling point': -88,
+        'code': 'nwchem'
     }
 
     body = {
@@ -223,7 +224,8 @@ def test_ingest_nwchem_pending(server, molecule, user, make_girder_file, fsAsset
                 'name': '3-21g'
             },
             'theory': 'b3lyp',
-            'pending': True
+            'pending': True,
+            'code': 'nwchem'
         }
     }
 
@@ -265,7 +267,10 @@ def test_ingest_nwchem_with_molecule(server, molecule, user, make_girder_file, f
     body = {
         'fileId': str(file['_id']),
         'moleculeId': str(molecule['_id']),
-        'public': True
+        'public': True,
+        'properties': {
+            'code': 'nwchem'
+        }
     }
 
     r = server.request('/calculations', method='POST', type='application/json',
@@ -288,7 +293,10 @@ def test_ingest_nwchem_without_molecule(server, molecule, user, make_girder_file
     # Now we can test the ingest
     body = {
         'fileId': str(file['_id']),
-        'public': True
+        'public': True,
+        'properties': {
+            'code': 'nwchem'
+        }
     }
 
     r = server.request('/calculations', method='POST', type='application/json',
