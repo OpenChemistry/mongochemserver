13054d547006f26be92aec8d836a33490bd36c40
Muammar El Khatib <melkhatibr@lbl.gov>
2019-03-06 11:03:46 -0800

    Addition of a nist module and changes to experiment's module
    With these changes it is possible to query a spectrum from NIST. This
    commit adds new dependencies: bs4 and jcamp.

--- girder/molecules/server/nist.py
+++ girder/molecules/server/nist.py
@@ -1,6 +1,5 @@
 import re
 import requests
-import os
 from bs4 import BeautifulSoup
 
 
@@ -23,28 +22,21 @@ def search_nist_inchi(inchi, stype='IR'):
     """
     EXACT_RE = re.compile('/cgi/cbook.cgi\?GetInChI=(.*?)$')
 
-    print('Searching: {}' .format(inchi))
     response = requests.get(NIST_URL, params={'InChI': inchi, 'Units': 'SI'})
     soup = BeautifulSoup(response.text)
     idlink = soup.find('a', href=EXACT_RE)
     if idlink:
         nistid = re.match(EXACT_RE, idlink['href']).group(1)
-        print('Result: {}' .format(nistid))
         return nistid
 
 
 def get_jdx(nistid, stype='IR'):
-    """Download jdx file for the specified NIST ID."""
-    filepath = os.path.join(JDX_PATH, '{}-{}.jdx' .format(nistid, stype))
-    if os.path.isfile(filepath):
-        print('{} {}: Already exists at {}' .format(nistid, stype, filepath))
-        return
-    print('{} {}: Downloading' .format(nistid, stype))
+    """Get jdx file content for the specified NIST ID."""
     response = requests.get(NIST_URL, params={'JCAMP': nistid, 'Type': stype,
                                               'Index': 0})
     if response.text == '##TITLE=Spectrum not found.\n##END=\n':
-        print('{} {}: Spectrum not found' .format(nistid, stype))
         return
-    print('Saving {}' .format(filepath))
-    with open(filepath, 'wb') as file:
-        file.write(response.content)
+    else:
+        content = response.content.splitlines()
+        content = [line.decode("utf-8") for line in content]
+        return content
