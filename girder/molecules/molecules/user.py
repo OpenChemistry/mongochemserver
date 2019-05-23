6408a99d65016d679bc16528c9e4df53cb3f5931
Patrick Avery <patrick.avery@kitware.com>
2019-05-23 17:59:02 -0400

    Fix some permissions for getting orcid/twitter
    Setting twitter/orcid requires admin level access for
    the user. Getting twitter/orcid only requires read level
    access for the user.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- girder/molecules/molecules/user.py
+++ girder/molecules/molecules/user.py
@@ -17,12 +17,14 @@ def _set_user_field(user, field_name, field_value):
 
     User().update(query, update)
 
-    return User().load(user['_id'], user=getCurrentUser())
+    # Get the updated user and return it
+    user = User().findOne(user['_id'])
+    return User().filter(user, getCurrentUser(), [field_name])
 
 @access.public
 @autoDescribeRoute(
     Description('Get the orcid of a user.')
-    .modelParam('id', 'The ID of the user.', model=User, level=AccessType.ADMIN)
+    .modelParam('id', 'The ID of the user.', model=User, level=AccessType.READ)
 )
 def get_orcid(user):
     return user.get('orcid')
@@ -30,7 +32,7 @@ def get_orcid(user):
 @access.user
 @autoDescribeRoute(
     Description('Set the orcid of a user.')
-    .modelParam('id', 'The ID of the user.', model=User, level=AccessType.READ)
+    .modelParam('id', 'The ID of the user.', model=User, level=AccessType.ADMIN)
     .param('orcid', 'The orcid to set.')
     .param('public', 'Whether or not the orcid is public.', dataType='boolean',
            required=False)
@@ -41,7 +43,7 @@ def set_orcid(user, orcid, public):
 @access.public
 @autoDescribeRoute(
     Description('Get the twitter username of a user.')
-    .modelParam('id', 'The ID of the user.', model=User, level=AccessType.ADMIN)
+    .modelParam('id', 'The ID of the user.', model=User, level=AccessType.READ)
 )
 def get_twitter(user):
     return user.get('twitter')
@@ -49,9 +51,9 @@ def get_twitter(user):
 @access.user
 @autoDescribeRoute(
     Description('Set the twitter username of a user.')
-    .modelParam('id', 'The ID of the user.', model=User, level=AccessType.READ)
-    .param('twitter', 'The twitter to set.')
-    .param('public', 'Whether or not the twitter is public.', dataType='boolean',
+    .modelParam('id', 'The ID of the user.', model=User, level=AccessType.ADMIN)
+    .param('twitter', 'The twitter username to set.')
+    .param('public', 'Whether or not the twitter username is public.', dataType='boolean',
            required=False)
 )
 def set_twitter(user, twitter, public):
