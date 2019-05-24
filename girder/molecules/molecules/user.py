804c5a6483a37179fff35d37f765157b97912d4c
Patrick Avery <patrick.avery@kitware.com>
2019-05-24 07:15:02 -0400

    Fix flake8 issues
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- girder/molecules/molecules/user.py
+++ girder/molecules/molecules/user.py
@@ -4,6 +4,7 @@ from girder.api.rest import getCurrentUser
 from girder.models.model_base import AccessType
 from girder.models.user import User
 
+
 def _set_user_field(user, field_name, field_value):
     query = {
         '_id': user['_id']
@@ -21,6 +22,7 @@ def _set_user_field(user, field_name, field_value):
     user = User().findOne(user['_id'])
     return User().filter(user, getCurrentUser(), [field_name])
 
+
 @access.public
 @autoDescribeRoute(
     Description('Get the orcid of a user.')
@@ -29,16 +31,19 @@ def _set_user_field(user, field_name, field_value):
 def get_orcid(user):
     return user.get('orcid')
 
+
 @access.user
 @autoDescribeRoute(
     Description('Set the orcid of a user.')
-    .modelParam('id', 'The ID of the user.', model=User, level=AccessType.ADMIN)
+    .modelParam('id', 'The ID of the user.', model=User,
+                level=AccessType.ADMIN)
     .param('orcid', 'The orcid to set.')
     .param('public', 'Whether or not the orcid is public.', dataType='boolean',
            required=False)
 )
 def set_orcid(user, orcid, public):
     return _set_user_field(user, 'orcid', orcid)
+
 
 @access.public
 @autoDescribeRoute(
@@ -48,13 +53,15 @@ def set_orcid(user, orcid, public):
 def get_twitter(user):
     return user.get('twitter')
 
+
 @access.user
 @autoDescribeRoute(
     Description('Set the twitter username of a user.')
-    .modelParam('id', 'The ID of the user.', model=User, level=AccessType.ADMIN)
+    .modelParam('id', 'The ID of the user.', model=User,
+                level=AccessType.ADMIN)
     .param('twitter', 'The twitter username to set.')
-    .param('public', 'Whether or not the twitter username is public.', dataType='boolean',
-           required=False)
+    .param('public', 'Whether or not the twitter username is public.',
+           dataType='boolean', required=False)
 )
 def set_twitter(user, twitter, public):
     return _set_user_field(user, 'twitter', twitter)
