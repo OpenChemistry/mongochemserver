9c63757062ce99e7efe8cfa086948ea6fcdbb0b9
Patrick Avery <patrick.avery@kitware.com>
2019-09-03 06:34:51 -0400

    GET geometries in different formats
    Allow geometries to be obtained in different formats.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- girder/molecules/molecules/geometry.py
+++ girder/molecules/molecules/geometry.py
@@ -1,3 +1,6 @@
+import cherrypy
+import json
+
 from girder.api.describe import Description, autoDescribeRoute
 from girder.api import access
 from girder.api.rest import Resource
@@ -9,13 +12,28 @@ from girder.constants import TokenScope
 
 from .models.geometry import Geometry as GeometryModel
 
+from molecules import avogadro
+
 
 class Geometry(Resource):
+
+    # No 2d formats are currently supported
+    output_formats_2d = []
+    output_formats_3d = ['cml', 'xyz', 'sdf', 'cjson']
+    output_formats = output_formats_2d + output_formats_3d
+
+    mime_types = {
+        'cml': 'chemical/x-cml',
+        'xyz': 'chemical/x-xyz',
+        'sdf': 'chemical/x-mdl-sdfile',
+        'cjson': 'application/json'
+    }
 
     def __init__(self):
         super(Geometry, self).__init__()
         self.resourceName = 'geometry'
         self.route('GET', (), self.find_geometries)
+        self.route('GET', (':id', ':output_format'), self.get_format)
         self.route('POST', (), self.create)
         self.route('DELETE', (':id',), self.delete)
 
@@ -56,6 +74,35 @@ class Geometry(Resource):
 
         return self._clean(self._model.create(user, moleculeId, cjson, 'user',
                                               user['_id']))
+
+    @access.public
+    @autoDescribeRoute(
+        Description('Get a geometry in a specified format.')
+        .param('id', 'The id of the geometry to be used.')
+        .param('output_format', 'The output format of the geometry.')
+        .errorResponse('Format not supported.')
+        .errorResponse('Geometry not found.', 404)
+    )
+    def get_format(self, id, output_format):
+
+        if output_format not in Geometry.output_formats:
+            raise RestException('Format not supported.')
+
+        user = getCurrentUser()
+        geometry = GeometryModel().load(id, level=AccessType.READ, user=user)
+
+        if not geometry:
+            raise RestException('Geometry not found.', code=404)
+
+        data = json.dumps(geometry['cjson'])
+        if output_format != 'cjson':
+            data = avogadro.convert_str(data, 'cjson', output_format)
+
+        def stream():
+            cherrypy.response.headers['Content-Type'] = Geometry.mime_types[output_format]
+            yield data
+
+        return stream
 
     @access.user(scope=TokenScope.DATA_WRITE)
     @autoDescribeRoute(
